232af47aed74a5b9a79d9fb4522421591e7b9650
bokyungseo <maybutter756@gmail.com>
2023-02-19 13:44:03 +0900

    initial: 프로젝트 생성

---
+++ PreOnboardingiOS/PreOnboardingiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PreOnboardingiOS
+//
+//  Created by 서보경 on 2023/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
